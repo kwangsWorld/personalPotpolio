a84d56a0248c469947bfd26242b8dd01221ac199
Kwangs <leekp7@naver.com>
2023-09-20 14:35:01 +0900

    백준 문제풀이

---
+++ baekjun/stage1/Cat.java
@@ -0,0 +1,15 @@
+package stage1;
+
+public class Cat {
+
+	public static void main(String[] args) {
+		
+		System.out.println("\\" + "    /\\");
+		System.out.println( " )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		
+	}
+
+}
