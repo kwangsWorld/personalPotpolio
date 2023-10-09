e3dbc2fa9bb77c7c41149c0b8689994886d5d2ec
Kwangs <leekp7@naver.com>
2023-10-09 20:02:20 +0900

    백준 문제풀이

---
+++ baekjun/stage3/Main10951.java
@@ -0,0 +1,17 @@
+package stage3;
+
+import java.util.Scanner;
+
+public class Main10951 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			System.out.println(num1 + num2);
+		}
+	}
+}
