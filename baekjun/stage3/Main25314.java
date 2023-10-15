3e0cc62af7e31ce0e5543ed5dbd33bd12350c18d
Kwangs <leekp7@naver.com>
2023-10-15 23:33:57 +0900

    백준 문제풀이

---
+++ baekjun/stage3/Main25314.java
@@ -0,0 +1,20 @@
+package stage3;
+
+import java.util.Scanner;
+public class Main25314 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int longCnt = n / 4;
+		
+		for(int i = 0; i < longCnt; i++) {
+			System.out.print("long ");
+		}
+		System.out.println("int");
+		
+		
+	}//main
+
+}//class
