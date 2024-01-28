6aee0fab116ab903c4ee3582943915d017339c0e
Kwangs <leekp7@naver.com>
2024-01-28 21:57:06 +0900

    js

---
+++ frontRepo/jsRepo/06_내장객체.js
@@ -0,0 +1,26 @@
+function f01(){
+    // const x = {};
+    // const x = new Object();
+
+    // const a = 10;
+    // const b = Number(10);
+    // const c = new Number(10);
+
+    // console.log(a);
+    // console.log(b);
+    // console.log(c);
+
+    // const str1 = "zzz";
+    // const str2 = new String("abc");
+
+    // console.log(str1);
+    // console.log(str2[2]);
+
+    // const x = new Date();.
+    const x = new Date("2023-12-31 10:30");
+
+    console.log(x);
+
+}
+
+f01();
