3f1113c1611182797e032b202a2602ba31ea46e2
Kwangs <leekp7@naver.com>
2024-03-06 10:56:53 +0900

    react

---
+++ wasRepo/khappex/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Wrap from './component/Wrap.js'
+
+function App() {
+  return (
+    <>
+      <Wrap/>
+    </>
+  );
+}
+
+export default App;
---
+++ wasRepo/khappex/src/component/Navigator.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+export default function Navigator(){
+
+    return (<>
+        <div className="select">
+            <Link to="/movie/list">List</Link>
+            <Link to="/movie/add">Add new Movie</Link>
+        </div>
+    </>);
+
+}
