8933c109eb7d306003f2beffc52b6443d590c393
Kwangs <leekp7@naver.com>
2024-03-13 14:12:05 +0900

    react

---
+++ intellijRepo/firstPrj/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("<..이소라..Tears...>\n" +
+                "좁고 어두운 방"
+                "이렇게 못나... 혼자.. 남아... 아직도 너를 그리워 해...\n" +
+                " 이렇게 나만... 서럽게 남아.. 반기지 않는 전화를 해!");
+
+
+    }
+}
